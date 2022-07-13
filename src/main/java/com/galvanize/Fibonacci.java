2b472e22c742216d21336c58149bbfc7c70bdf76
Yu Zhang <rita890927@gmail.com>
2022-07-13 13:06:29 -0400

    Initial code

---
+++ src/main/java/com/galvanize/Fibonacci.java
@@ -0,0 +1,15 @@
+package com.galvanize;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int num = 10;
+        int prev = 1;
+        int cur = 1;
+
+        for (int n = 2; n < num; n++) {
+
+
+        }
+    }
+}
+
