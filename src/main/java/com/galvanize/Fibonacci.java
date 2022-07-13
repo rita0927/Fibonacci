533a5b399f295f3d60d76c661a90fb3e33e7c21f
Yu Zhang <rita890927@gmail.com>
2022-07-13 14:07:41 -0400

    add method

--- src/main/java/com/galvanize/Fibonacci.java
+++ src/main/java/com/galvanize/Fibonacci.java
@@ -1,10 +1,11 @@
 package com.galvanize;
 
 public class Fibonacci {
-    public static void main(String[] args) {
-        int num = 2;
-        if (num < 2) {
-            System.out.println(1);
+    public static Boolean runFiobonacci(int num, int target) {
+
+
+        if (num > 0 && target == 1) {
+            return true;
         } else if (num <= 0) {
             System.out.println("Please enter a valid integer");
         } else {
@@ -12,13 +13,28 @@ public class Fibonacci {
             int cur = 1;
             int temp;
 
-            for (int n = 2; n < num; n++) {
+            for (int index = 2; index <= num; index++) {
+
                 temp = prev;
                 prev = cur;
                 cur += temp;
+
+                if (cur > target) {
+                    return false;
+                } else if (cur == target) {
+                    return true;
+                }
             }
-            System.out.println(cur);
         }
+        return false;
+
+
+    }
+
+    public static void main(String[] args) {
+        int num = 2;
+        int target = 5;
+        System.out.println(runFiobonacci(num, target));
     }
 }
 
