940d0457e575238af4a9c27db299af3b5c9d7cc8
deleonenriqueta <enriqueta.correa@gmail.com>
2022-07-13 10:24:36 -0700

    completed problem

--- src/main/java/com/galvanize/Fibonacci.java
+++ src/main/java/com/galvanize/Fibonacci.java
@@ -2,14 +2,24 @@ package com.galvanize;
 
 public class Fibonacci {
     public static void main(String[] args) {
-        int num = 10;
-        int prev = 1;
-        int cur = 1;
+        int num = 2;
+        if (num < 2) {
+            System.out.println(1);
+        } else if (num <= 0) {
+            System.out.println("Please enter a valid integer");
+        } else {
+            int prev = 1;
+            int cur = 1;
+            int temp;
 
-        for (int n = 2; n < num; n++) {
-
-
+            for (int n = 2; n < num; n++) {
+                temp = prev;
+                prev = cur;
+                cur += temp;
+            }
+            System.out.println(cur);
         }
     }
 }
 
+//  1 1 2 3 5
